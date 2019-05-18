1a50374407cb70d6e8e15e6dcaec29c62c88320b
OnescuO <onescu.oana@gmail.com>
2019-05-18 12:30:39 +0300

    added a project file

---
+++ string/ReadStuff/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ReadStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a string");
+            string readedLine = Console.ReadLine();
+
+
+            Console.WriteLine("please enter a string");
+            string readedLine2 = Console.ReadLine();
+
+            if (readedLine == readedLine2)
+            {
+                Console.WriteLine("ok");
+            }
+
+            // compare
+            Console.WriteLine(string.Compare(readedLine, readedLine2));
+            if (readedLine.Equals(readedLine2)) 
+            {
+                Console.WriteLine("equals");
+            }
+
+
+        }
+    }
+}
